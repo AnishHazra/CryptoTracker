b8d0257b5e3b3936f6fca33ff27bc5d8f4c16d85
AnishCodes <anishhazra108@gmail.com>
2025-05-16 20:24:18 +0530

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Anish Hazra on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
