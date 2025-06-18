cc486572a94d28e0f50e4603aa18d68967c36c9c
AnishCodes <anishhazra108@gmail.com>
2025-06-18 21:14:30 +0530

    added search filter logic working

--- CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -5,14 +5,14 @@
 //  Created by Anish Hazra on 21/05/25.
 //
 
-import Foundation
 import Combine
+import Foundation
 
 class HomeViewModel: ObservableObject {
 
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
-    
+
     @Published var searchText: String = ""
 
     private let dataService = CoinDataService()
@@ -24,12 +24,30 @@ class HomeViewModel: ObservableObject {
     }
 
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] (returnedCoins) in
+        
+        // Updates all coins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main) // slight delay for fast typing
+            .map(filterCoins)
+            .sink{ [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
-
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText)
+                || coin.symbol.lowercased().contains(lowercasedText)
+                || coin.id.lowercased().contains(lowercasedText)
+        }
     }
 
 }
